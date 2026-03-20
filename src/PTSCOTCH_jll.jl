72fe46ca3ae379d9c8947284cbe4b50be685b2fc
jlbuild <juliabuildbot@gmail.com>
2026-03-20 15:44:25 +0000

    PTSCOTCH_jll build 7.0.5+0

--- src/PTSCOTCH_jll.jl
+++ src/PTSCOTCH_jll.jl
@@ -8,5 +8,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("PTSCOTCH")
-JLLWrappers.@generate_main_file("PTSCOTCH", UUID("b3ec0f5a-9838-5c9b-9e77-5f2c6a4b089f"))
+JLLWrappers.@generate_main_file("PTSCOTCH", Base.UUID("b3ec0f5a-9838-5c9b-9e77-5f2c6a4b089f"))
 end  # module PTSCOTCH_jll
